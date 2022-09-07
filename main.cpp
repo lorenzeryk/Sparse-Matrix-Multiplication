fcd45b2d91d145d934aa8aa2671264a31a5dd5fe
Eryk Lorenz <eryklorenz@mines.edu>
2022-09-06 20:08:51 -0600

    Add gitignore

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+
+void loadFile();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    loadFile();
     return 0;
+}
+
+void loadFile() {
+    std::ifstream fileLoader;
+    fileLoader.open("NLR.mtx");
+    while (fileLoader.is_open()) {
+        bool commentedLines = true;
+        std::string test;
+        fileLoader >> test;
+        char *currentLine;
+        while (commentedLines) {
+            fileLoader.getline(currentLine, 256);
+            char test = *currentLine;
+            commentedLines = false;
+        }
+    }
 }
